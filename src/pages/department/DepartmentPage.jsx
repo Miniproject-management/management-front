fffb604086d59e30840047019f61bf4890ae623b
hjyouns <hjyouns@gmail.com>
2026-05-12 13:06:58 +0900

    css update

--- src/pages/department/DepartmentPage.jsx
+++ src/pages/department/DepartmentPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './DepartmentPage.css';
+import '../../styles/DepartmentPage.css';
 
 function DepartmentPage() {
   const [departments, setDepartments] = useState([]);
