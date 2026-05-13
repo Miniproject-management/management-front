365c5b2693fdf5d0821fe5c685396584d43ac9ee
Hyein <hi020514@yourdomain.com>
2026-05-13 16:46:46 +0900

    feat: 로그인 페이지 수정

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -7,17 +7,21 @@ import { loginApi } from "../../api/authApi";
 import useAuthStore from "../../stores/authStore";
 
 function LoginPage() {
+
   const navigate = useNavigate();
 
   const [empNo, setEmpNo] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = useAuthStore((state) => state.login);
+  const login =
+    useAuthStore((state) => state.login);
 
   const handleLogin = async (e) => {
+
     e.preventDefault();
 
     try {
+
       const data = await loginApi({
         empNo,
         password,
@@ -28,25 +32,42 @@ function LoginPage() {
       alert("로그인 성공");
 
       navigate("/");
+
     } catch (error) {
+
       console.error(error);
 
-      alert("로그인 실패");
+      // 백엔드 에러 메시지 출력
+      alert(
+        error.response?.data?.message
+        || "로그인 실패"
+      );
     }
   };
 
   return (
     <div className="login-page">
+
+      {/* 브랜드 영역 */}
       <div className="login-brand">
+
         <img
           src="/hellohr-logo.png"
           alt="HELLO HR"
           className="login-brand__logo"
         />
-        <span className="login-brand__name">HELLO HR</span>
-        <p className="login-brand__slogan">당신의 안전한 안녕이 시작됩니다.</p>
+
+        <span className="login-brand__name">
+          HELLO HR
+        </span>
+
+        <p className="login-brand__slogan">
+          당신의 안전한 안녕이 시작됩니다.
+        </p>
+
       </div>
 
+      {/* 로그인 카드 */}
       <div className="login-container">
 
         {/* 제목 */}
@@ -54,7 +75,7 @@ function LoginPage() {
           로그인
         </h1>
 
-        {/* 설명 문구 추가 */}
+        {/* 설명 */}
         <p className="login-description">
           사번과 비밀번호를 입력해주세요.
         </p>
@@ -63,7 +84,10 @@ function LoginPage() {
           className="login-form"
           onSubmit={handleLogin}
         >
+
+          {/* 사번 */}
           <div className="input-group">
+
             <label>사번</label>
 
             <input
@@ -74,9 +98,12 @@ function LoginPage() {
                 setEmpNo(e.target.value)
               }
             />
+
           </div>
 
+          {/* 비밀번호 */}
           <div className="input-group">
+
             <label>비밀번호</label>
 
             <input
@@ -87,14 +114,17 @@ function LoginPage() {
                 setPassword(e.target.value)
               }
             />
+
           </div>
 
+          {/* 로그인 버튼 */}
           <button
             type="submit"
             className="login-button"
           >
             로그인
           </button>
+
         </form>
 
       </div>
